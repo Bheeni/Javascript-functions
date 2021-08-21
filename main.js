fdf6c0ca201b41dcd3248981a40ca54a300877c3
Bheeni <84077152+Bheeni@users.noreply.github.com>
2021-08-21 17:46:15 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,23 @@
+ var first_name = "Ranbir ";
+ console.log(first_name);
+
+ var last_name = "Kapoor";
+ console.log(last_name);
+
+ var full_name = first_name .concat(last_name);
+ console.log(full_name);
+
+ var lenth_of_full_name = full_name.length;
+ console.log(lenth_of_full_name);
+
+ var sentence_part1 = "My first project";
+ console.log(sentence_part1);
+
+ var sentence_part2 = " on JavaScript";
+ console.log(sentence_part2);
+
+ var full_sentence = sentence_part1 .concat(sentence_part2);
+ console.log(full_sentence);
+
+ var lenth_of_full_sentence = full_sentence.length;
+ console.log(lenth_of_full_sentence);
